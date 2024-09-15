4c35f5b5247ded44cffe2b6ef42e3cea317a0bc2
Seye Yemi-Olowolabi <sammybammystudios@gmail.com>
2024-09-15 13:30:49 +0100

    added an npm package to the project directory

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import Header from '../../components/Header'
+import React from "react";
+import Header from "../../components/Header";
+import CurrencyInput from "react-currency-input-field";
 
 const Home = () => {
   return (
     <>
-    <Header/>
+      <Header />
 
-    <h4>Home</h4>
+      <h4>Home</h4>
+      <div>
+        <CurrencyInput
+          id="input-example"
+          name="input-name"
+          placeholder="Please enter a number"
+          className="py-3 px-3 w-[200px] border-2 focus:border-purple-600 acti"
+          prefix="N"
+          defaultValue={1000}
+          decimalsLimit={2}
+          onValueChange={(value, name, values) =>
+            console.log(value, name, values)
+          }
+        />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
